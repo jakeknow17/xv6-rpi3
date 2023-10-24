ad9a25d82d743d949f9c54aaf3dcbf7560a883eb
Jacob Knowlton <knowlton.jacob17@gmail.com>
2023-10-24 14:03:11 -0600

    Added uart code from tutorial

--- kernel/start.c
+++ kernel/start.c
@@ -1,5 +1,7 @@
 #include "types.h"
 #include "param.h"
+#include "aarch64.h"
+#include "defs.h"
 
 // void main();
 
@@ -9,8 +11,25 @@ __attribute__ ((aligned (16))) char stack0[4096 * NCPU];
 
 void start()
 {
-    while(1)
-    {
-        // do nothing
-    }
+    uart_init();
+    uart_puts("Hello World!\n");
+    uart_puts("Second time!\n");
+    char txt[3];
+    txt[0] = (char)r_mpidr_el1();
+    txt[1] = '\n';
+    txt[2] = 0;
+    uart_puts(txt);
+    if (txt[0] == 0)
+        uart_puts("Zero!\n");
+    else
+        uart_puts("Else!\n");
+    // if (r_mpidr_el1() == 0)
+    // {
+    //     uart_init();
+    //     uart_puts("Hello World!\n");
+    // }
+    // else
+    // {
+    //     while(1);
+    // }
 }
