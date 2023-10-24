c59b36abdce17a8d1b8bae1b028153513e4f312f
Jacob Knowlton <knowlton.jacob17@gmail.com>
2023-10-24 01:11:01 -0600

    Added startup files

---
+++ kernel/start.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "param.h"
+
+// void main();
+
+// entry.S needs one stack per CPU.
+// __attribute__ ((aligned (16))) char stack0[4096 * NCPU];
+char stack0[4096 * NCPU];
+
+void
+start()
+{
+    while(1)
+    {
+        // do nothing
+    }
+}
