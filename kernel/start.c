899e944fdac6cf234c69ff471246fdd72e1bb096
Jacob Knowlton <knowlton.jacob17@gmail.com>
2023-10-24 23:52:47 -0600

    Fixed issue with mpidr_el1

--- kernel/start.c
+++ kernel/start.c
@@ -6,30 +6,19 @@
 // void main();
 
 // entry.S needs one stack per CPU.
-__attribute__ ((aligned (16))) char stack0[4096 * NCPU];
+// __attribute__ ((aligned (16))) char stack0[4096 * NCPU];
 // char stack0[4096 * NCPU];
 
 void start()
 {
-    uart_init();
-    uart_puts("Hello World!\n");
-    uart_puts("Second time!\n");
-    char txt[3];
-    txt[0] = (char)r_mpidr_el1();
-    txt[1] = '\n';
-    txt[2] = 0;
-    uart_puts(txt);
-    if (txt[0] == 0)
-        uart_puts("Zero!\n");
+    if (!(r_mpidr_el1() & 0x3))
+    {
+        uart_init();
+        uart_puts("Hello World!\n");
+        uart_puts("Second time!\n");
+    }
     else
-        uart_puts("Else!\n");
-    // if (r_mpidr_el1() == 0)
-    // {
-    //     uart_init();
-    //     uart_puts("Hello World!\n");
-    // }
-    // else
-    // {
-    //     while(1);
-    // }
+    {
+        return;
+    }
 }
