1cfc204e3d85f5dfc11a9bf020d76f2d5e1afd07
Jacob Knowlton <knowlton.jacob17@gmail.com>
2023-10-25 23:42:34 -0600

    Changed from uart1 to uart0

--- kernel/uart.c
+++ kernel/uart.c
@@ -1,28 +1,43 @@
 #include "memlayout.h"
+#include "types.h"
+#include "mailbox.h"
 
 void uart_init()
 {
     register unsigned int r;
 
     /* initialize UART */
-    *AUX_ENABLES |=1;       // enable UART1, AUX mini uart
-    *AUX_MU_CNTL_REG = 0;
-    *AUX_MU_LCR_REG = 3;       // 8 bits
-    *AUX_MU_MCR_REG = 0;
-    *AUX_MU_IER_REG = 0;
-    *AUX_MU_IIR_REG = 0xc6;    // disable interrupts
-    *AUX_MU_BAUD_REG = 270;    // 115200 baud
-    /* map UART1 to GPIO pins */
+    *PL011_CR = 0;         // turn off UART0
+
+    /* set up clock for consistent divisor values */
+    __attribute__ ((aligned (16))) uint32 mbox[9];
+    mbox[0] = 9*4;
+    mbox[1] = MBOX_REQUEST;
+    mbox[2] = MBOX_CLK_SET_CLK_RATE; // set clock rate
+    mbox[3] = 12;
+    mbox[4] = 8;
+    mbox[5] = 2;           // UART clock
+    mbox[6] = 4000000;     // 4Mhz
+    mbox[7] = 0;           // clear turbo
+    mbox[8] = MBOX_TAG_LAST;
+    write_mailbox(MBOX_CH_PROP, mbox);
+
+    /* map UART0 to GPIO pins */
     r=*GPFSEL1;
     r&=~((7<<12)|(7<<15)); // gpio14, gpio15
-    r|=(2<<12)|(2<<15);    // alt5
+    r|=(4<<12)|(4<<15);    // alt0
     *GPFSEL1 = r;
     *GPPUD = 0;            // enable pins 14 and 15
     r=150; while(r--) { asm volatile("nop"); }
     *GPPUDCLK0 = (1<<14)|(1<<15);
     r=150; while(r--) { asm volatile("nop"); }
     *GPPUDCLK0 = 0;        // flush GPIO setup
-    *AUX_MU_CNTL_REG = 3;      // enable Tx, Rx
+
+    *PL011_ICR = 0x7FF;    // clear interrupts
+    *PL011_IBRD = 2;       // 115200 baud
+    *PL011_FBRD = 0xB;
+    *PL011_LCRH = 0x7<<4;  // 8n1, enable FIFOs
+    *PL011_CR = 0x301;     // enable Tx, Rx, UART
 }
 
 /**
@@ -30,18 +45,21 @@ void uart_init()
  */
 void uart_send(unsigned int c) {
     /* wait until we can send */
-    do{asm volatile("nop");}while(!(*AUX_MU_LSR_REG&0x20));
+    do{asm volatile("nop");}while(*PL011_FR&0x20);
     /* write the character to the buffer */
-    *AUX_MU_IO_REG=c;
+    *PL011_DR=c;
 }
 
+/**
+ * Receive a character
+ */
 char uart_getc() {
     char r;
     /* wait until something is in the buffer */
-    do{asm volatile("nop");}while(!(*AUX_MU_LSR_REG&0x01));
+    do{asm volatile("nop");}while(*PL011_FR&0x10);
     /* read it and return */
-    r=(char)(*AUX_MU_IO_REG);
-    /* convert carriage return to newline */
+    r=(char)(*PL011_DR);
+    /* convert carrige return to newline */
     return r=='\r'?'\n':r;
 }
 
@@ -50,9 +68,37 @@ char uart_getc() {
  */
 void uart_puts(char *s) {
     while(*s) {
-        /* convert newline to carriage return + newline */
+        /* convert newline to carrige return + newline */
         if(*s=='\n')
             uart_send('\r');
         uart_send(*s++);
     }
+}
+
+/**
+ * Display a binary value in hexadecimal
+ */
+void uart_hex(unsigned int d, int c) {
+    unsigned int n;
+    for(;c>=0;c-=4) {
+        // get highest tetrad
+        n=(d>>c)&0xF;
+        // 0-9 => '0'-'9', 10-15 => 'A'-'F'
+        n+=n>9?0x37:0x30;
+        uart_send(n);
+    }
+}
+
+/**
+ * Display a 32-bit binary value in hexadecimal
+ */
+void uart_hex_u32(unsigned int d) {
+    uart_hex((unsigned)d, 28); // 32 - 4
+}
+
+/**
+ * Display a 64-bit binary value in hexadecimal
+ */
+void uart_hex_u64(unsigned long d) {
+    uart_hex(d, 60); // 64 - 4
 }
