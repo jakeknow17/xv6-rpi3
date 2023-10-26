71d62bd88991f92db31b57d4bd3c62c55e93e44b
Jacob Knowlton <knowlton.jacob17@gmail.com>
2023-10-25 23:42:18 -0600

    Added printing of board serial number

--- kernel/start.c
+++ kernel/start.c
@@ -2,12 +2,9 @@
 #include "param.h"
 #include "aarch64.h"
 #include "defs.h"
+#include "mailbox.h"
 
 // void main();
-
-// entry.S needs one stack per CPU.
-// __attribute__ ((aligned (16))) char stack0[4096 * NCPU];
-// char stack0[4096 * NCPU];
 
 void start()
 {
@@ -17,7 +14,7 @@ void start()
 
         uart_puts("Hello World!\n");
         uart_puts("Second time!\n");
-
+        uart_hex_u64(get_board_serial());
         // echo
         while(1)
             uart_send(uart_getc());
